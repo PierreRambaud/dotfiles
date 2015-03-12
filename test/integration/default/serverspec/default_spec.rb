84e3730a3a150bd2ca7ce077a056815c5df86e2f
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2015-03-12 13:30:47 +0100

    Add context for kitchen tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,49 +2,53 @@
 #
 require 'spec_helper'
 
-%w(gitconfig gitignore bashrc bash_aliases).each do |item|
-  describe file("/home/got/.#{item}") do
+context '#config' do
+  %w(gitconfig gitignore bashrc bash_aliases).each do |item|
+    describe file("/home/got/.#{item}") do
+      it { should be_file }
+      it { should be_grouped_into 'got' }
+      it { should be_owned_by 'got' }
+    end
+  end
+
+  %w(config/terminator conkyrc fluxbox emacs.d emacs.d/.cask emacs.d/prelude).each do |path|
+    describe file("/home/got/.#{path}") do
+      it { should be_directory }
+      it { should be_grouped_into 'got' }
+      it { should be_owned_by 'got' }
+    end
+  end
+
+  %w(conkyrc/got config/terminator/config).each do |item|
+    describe file("/home/got/.#{item}") do
+      it { should be_file }
+      it { should be_grouped_into 'got' }
+      it { should be_owned_by 'got' }
+      its(:content) { should match(/D00402/) }
+    end
+  end
+
+  %w(fluxbox/init).each do |item|
+    describe file("/home/got/.#{item}") do
+      it { should be_file }
+      it { should be_grouped_into 'got' }
+      it { should be_owned_by 'got' }
+      its(:content) { should match(/GotRedBlack/) }
+    end
+  end
+end
+
+context '#tools' do
+  describe file('/home/got/.fluxbox/scripts/move-window.sh') do
     it { should be_file }
+    it { should be_executable }
     it { should be_grouped_into 'got' }
     it { should be_owned_by 'got' }
   end
-end
 
-%w(config/terminator conkyrc fluxbox emacs.d emacs.d/.cask emacs.d/prelude).each do |path|
-  describe file("/home/got/.#{path}") do
-    it { should be_directory }
-    it { should be_grouped_into 'got' }
-    it { should be_owned_by 'got' }
-  end
-end
-
-describe file('/home/got/.fluxbox/scripts/move-window.sh') do
-  it { should be_file }
-  it { should be_executable }
-  it { should be_grouped_into 'got' }
-  it { should be_owned_by 'got' }
-end
-
-%w(emacs rbenv conky fluxbox pyenv).each do |item|
-  describe command("type #{item}") do
-    its(:exit_status) { should eq 0 }
-  end
-end
-
-%w(conkyrc/got config/terminator/config).each do |item|
-  describe file("/home/got/.#{item}") do
-    it { should be_file }
-    it { should be_grouped_into 'got' }
-    it { should be_owned_by 'got' }
-    its(:content) { should match(/D00402/) }
-  end
-end
-
-%w(fluxbox/init).each do |item|
-  describe file("/home/got/.#{item}") do
-    it { should be_file }
-    it { should be_grouped_into 'got' }
-    it { should be_owned_by 'got' }
-    its(:content) { should match(/GotRedBlack/) }
+  %w(emacs rbenv conky fluxbox pyenv).each do |item|
+    describe command("type #{item}") do
+      its(:exit_status) { should eq 0 }
+    end
   end
 end
--- test/integration/default/serverspec/rbenv_spec.rb
+++ test/integration/default/serverspec/rbenv_spec.rb
@@ -2,26 +2,28 @@
 #
 require 'spec_helper'
 
-['1.9.3-p551', '2.0.0-p598', '2.1.5'].each do |version|
-  describe file("/home/got/.rbenv/versions/#{version}") do
-    it { should be_directory }
-    it { should be_grouped_into 'got' }
-    it { should be_owned_by 'got' }
-  end
-
-  ['bundler'].each do |pkg|
-    describe file("/home/got/.rbenv/versions/#{version}/bin/#{pkg}") do
-      it { should be_file }
-      it { should be_executable }
+context '#rbenv' do
+  ['1.9.3-p551', '2.0.0-p598', '2.1.5'].each do |version|
+    describe file("/home/got/.rbenv/versions/#{version}") do
+      it { should be_directory }
       it { should be_grouped_into 'got' }
       it { should be_owned_by 'got' }
     end
-  end
-end
 
-describe file('/home/got/.rbenv/version') do
-  it { should be_file }
-  it { should be_grouped_into 'got' }
-  it { should be_owned_by 'got' }
-  its(:content) { should match(/2.1.5/) }
+    ['bundler'].each do |pkg|
+      describe file("/home/got/.rbenv/versions/#{version}/bin/#{pkg}") do
+        it { should be_file }
+        it { should be_executable }
+        it { should be_grouped_into 'got' }
+        it { should be_owned_by 'got' }
+      end
+    end
+  end
+
+  describe file('/home/got/.rbenv/version') do
+    it { should be_file }
+    it { should be_grouped_into 'got' }
+    it { should be_owned_by 'got' }
+    its(:content) { should match(/2.1.5/) }
+  end
 end

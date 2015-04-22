77ece5a53d4e2d2bf7225eaadb27642f772a54da
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-04-22 08:30:26 +0200

    Fix tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
 include_recipe 'apt'
-resources('package[update-notifier-common]').instance_exec do
-  action :nothing
-end
-
 include_recipe 'emacs24'
 include_recipe 'dotfiles::install'
 include_recipe 'dotfiles::config'

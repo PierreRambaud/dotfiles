ea800af872c331895eafea425d109651577ccc8d
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2017-02-23 17:54:38 +0100

    Add pyenv virtualenv, and fix gitignore file

--- cookbooks/dotfiles/recipes/install_pyenv.rb
+++ cookbooks/dotfiles/recipes/install_pyenv.rb
@@ -9,3 +9,12 @@ node.default['pyenv']['user_installs'] = [
 ]
 
 include_recipe 'pyenv::user'
+
+git 'install-pyenv-virtual-env' do
+  destination "#{node['dotfiles']['user_home']}/plugins/pyenv-virtualenv"
+  repository 'https://github.com/yyuu/pyenv-virtualenv.git'
+  user node['dotfiles']['user']
+  group node['dotfiles']['user']
+  timeout 9000
+  action :sync
+end

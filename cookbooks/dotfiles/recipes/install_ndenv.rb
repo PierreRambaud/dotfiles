5fdc4119ba4362675d9a58fbf4933e2eb9a1cc2f
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2017-10-11 10:05:27 +0200

    Change node versions

--- cookbooks/dotfiles/recipes/install_ndenv.rb
+++ cookbooks/dotfiles/recipes/install_ndenv.rb
@@ -2,8 +2,8 @@
 
 node.default['ndenv']['user'] = node['dotfiles']['user']
 node.default['ndenv']['user_home'] = node['dotfiles']['user_home']
-node.default['ndenv']['installs'] = %w[v8.1.2]
-node.default['ndenv']['global'] = 'v8.1.2'
+node.default['ndenv']['installs'] = %w[v8.6.0]
+node.default['ndenv']['global'] = 'v8.6.0'
 
 include_recipe 'ndenv'
 include_recipe 'ndenv::install'

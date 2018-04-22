aa66756d175a7b99ca1da7051cedccd2fa216998
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-04-22 09:54:22 +0200

    Update berks dependencies and remove python version

--- cookbooks/dotfiles/recipes/install_pyenv.rb
+++ cookbooks/dotfiles/recipes/install_pyenv.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
-py_version = '3.6.1'
-
 pyenv_user_install node['dotfiles']['user']
-
-pyenv_python py_version do
-  user node['dotfiles']['user']
-end
-
-pyenv_global py_version do
-  user node['dotfiles']['user']
-end
 
 pyenv_plugin 'virtualenv' do
   git_url 'https://github.com/yyuu/pyenv-virtualenv'

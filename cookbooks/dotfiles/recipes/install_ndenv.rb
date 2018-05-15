4c5a3c9d52ce2c1b1b3f8e2110770dd7e2f6f726
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-05-15 14:33:32 +0200

    Update Ndenv configuration

--- cookbooks/dotfiles/recipes/install_ndenv.rb
+++ cookbooks/dotfiles/recipes/install_ndenv.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-node.default['ndenv']['user'] = node['dotfiles']['user']
+node.default['ndenv']['group_users'] = [node['dotfiles']['user']]
 node.default['ndenv']['user_home'] = node['dotfiles']['user_home']
-node.default['ndenv']['installs'] = %w[v8.6.0]
-node.default['ndenv']['global'] = 'v8.6.0'
+node.default['ndenv']['installs'] = %w[v8.11.1 v10.1.0 v9.11.1]
+node.default['ndenv']['global'] = 'v8.11.1'
 
 include_recipe 'ndenv'
 include_recipe 'ndenv::install'
